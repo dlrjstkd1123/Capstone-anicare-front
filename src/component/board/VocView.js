a5f4d164a440ebae352feaaf03669c15770a57d2
HyumJun Byun <byjun9804@gmail.com>
2024-05-15 15:09:38 +0900

    상세조회 수정

--- src/component/board/VocView.js
+++ src/component/board/VocView.js
@@ -1,57 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
 import './VocView.css';
 
-function GetData(vocId) {
-  const [question, setQuestion] = useState({});
-  const [answer, setAnswer] = useState({});
+function VocView() {
+  const { vocId } = useParams();  // URL 매개변수에서 vocId를 가져옵니다
+  const [post, setPost] = useState({});
 
   useEffect(() => {
-    axios.get('http://3.38.225.120:8080/api/post/{id}').then((response)=> {
-        setQuestion(response.data.question);
-        setAnswer(response.data.answer);
-    })
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://3.38.225.120:8080/api/post/${vocId}`);
+        setPost(response.data.response); // response.data.response에서 post 데이터 추출
+      } catch (error) {
+        console.error("Error fetching the post data:", error);
+      }
+    };
 
-  const item =  (<>
-    <h2 align="center">게시글 상세정보</h2>
-    <div className="voc-view-wrapper">
-        <div className="voc-view-row">
-            <label>게시글 번호</label>
-            <label>{ question.id }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>제목</label>
-            <label>{ question.title }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>작성일</label>
-            <label>{ question.createDate }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>내용</label>
-            <div>
-                {
-                question.content
-                }
-            </div>
-        </div>
-    </div></>)
+    fetchData();
+  }, [vocId]);
 
-    return item;
+  return (
+    <>
+      <h2 align="center">게시글 상세정보</h2>
+      <div className="voc-view-wrapper">
+        <div className="voc-view-row">
+          <label>게시글 번호</label>
+          <label>{post.id}</label>
+        </div>
+        <div className="voc-view-row">
+          <label>제목</label>
+          <label>{post.title}</label>
+        </div>
+        <div className="voc-view-row">
+          <label>작성일</label>
+          <label>{post.createdAt}</label>
+        </div>
+        <div className="voc-view-row">
+          <label>내용</label>
+          <div>
+            {post.contents}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-function VocView() {
-  const{vocId} = useParams();
-  const item = GetData(vocId);
-
-  return (<>
-    <div>
-        {item}
-    </div>
-  </>);
-}
-  
 export default VocView;
